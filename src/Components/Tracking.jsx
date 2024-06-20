4f00cd9fcac6d15df0cb8c526c954e46ea47f5cb
Emmanuel <rodrigofemi@gmail.com>
2024-06-20 13:44:25 +0100

    Project Tracking

---
+++ src/Components/Tracking.jsx
@@ -0,0 +1,40 @@
+export const Tracking = () => {
+  return (
+    <div>
+      <header>
+        <img src="" alt="" />
+
+        <nav>
+          <li>
+            <a href="#">Product</a>
+          </li>
+          <li>
+            <a href="#">Features</a>
+          </li>
+          <li>
+            <a href="#">Pricing</a>
+          </li>
+        </nav>
+
+        <span>
+          <a href="">Login</a>
+        </span>
+      </header>
+
+      <div className={style.body}>
+        <div className={style.left}>
+          <div>
+            <button>New</button>
+            <p>Monograph Dashboard</p>
+          </div>
+
+          
+        </div>
+
+        <div className={style.right}>
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
